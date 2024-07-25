4237137190aab1b21e443eda5fe3bc0821553e5d
Shaista.kumbhar@gmail.com <Shaista.kumbhar2002@gmail.com>
2024-07-25 17:58:13 +0500

    Remove deleted files from the repository

--- eventModule.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('node:events');
-class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
-myEmitter.on('event', (a, b) => {
-    console.log(a, b, this);
-    // Prints: a b {}
-});
-myEmitter.emit('event', 'a', 'b');
