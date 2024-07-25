d0fa1b5d973a1652f56aa1e478784693bf32c762
Shaista.kumbhar@gmail.com <Shaista.kumbhar2002@gmail.com>
2024-07-25 17:13:58 +0500

    Changes in main files and Register file

--- Register.js
+++ Register.js
@@ -5,11 +5,13 @@ const dbConnection = require('./mongoose');
 
 dbConnection()
 
-router.post ('/Register', async(req, res) => {
+router.post('/Register', async(req, res) => {
     try {
         const { name,email,password } = req.body;
         const newUser= await User.create({
-           name, email, password
+            name:name,
+            email:email,
+            password:password
         })
         // const saved= await newUser.save();
         res.json({ success: true, User: newUser });
@@ -17,6 +19,17 @@ router.post ('/Register', async(req, res) => {
     }catch (e) {
         console.error(e);
         res.json({ success: false });
+    }
+})
+router.post('/login',async(req, res) => {
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email, password});
+        if(user){
+            res.send("Welcome")
+        }else res.send("Wrong email or password");
+    }catch (e) {
+        console.error(e);
     }
 })
 
