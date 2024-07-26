f50ed06cf26fbed2d1b6f1821a44addd8e2e4651
Shaista.kumbhar@gmail.com <Shaista.kumbhar2002@gmail.com>
2024-07-26 18:36:53 +0500

    changed directories

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const Register=require('./Register');
+const Register=require('./router/Register');
 const port = process.env.PORT || 3000;
 const dbConnection = require('./mongoose');
 
