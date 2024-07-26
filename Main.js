c603a8a2b33c37bbfce34412bb97af388856a5e5
Shaista.kumbhar@gmail.com <Shaista.kumbhar2002@gmail.com>
2024-07-26 18:32:44 +0500

    Stop tracking .gitignore

--- Main.js
+++ Main.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const Register=require('./Register');
 const port = process.env.PORT || 3000;
+const dbConnection = require('./mongoose');
 
+dbConnection()
 // //express.json() middleware to your Express app. This middleware will parse incoming requests with JSON payloads.
 app.use(express.json())
 
